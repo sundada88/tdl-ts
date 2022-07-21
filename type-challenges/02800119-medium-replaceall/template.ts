709d560276e195a86afd8af90302b3bb7a31f212
lane.sun <lane.sun@zoom.us>
2022-07-21 09:37:55 +0800

    add ts

---
+++ type-challenges/02800119-medium-replaceall/template.ts
@@ -0,0 +1,1 @@
+type ReplaceAll<S extends string, From extends string, To extends string> = From extends '' ? S : S extends `${infer Fir}${From}${infer Rest}` ? `${Fir}${To}${ReplaceAll<Rest, From, To>}` : S
