a69d8c9cf17d50a77feded1d4567d443178114c0
lane.sun <lane.sun@zoom.us>
2022-08-10 20:04:16 +0800

    add ts

--- type-challenges/other-ts-challenges.ts
+++ type-challenges/other-ts-challenges.ts
@@ -65,11 +65,24 @@
 
     // type UnionToTuple<T> = UnionToIntersection<T extends any ? () => T : never> extends () => infer ReturnType ? [...UnionToTuple<Exclude<T, ReturnType>>, ReturnType] : []
 
-    type UnionToIntersection<T> = (T extends T ? (x: T) => unknown) extends (x: infer R) => unknown ? R : never
+    type UnionToIntersection<T> = (T extends T ? (x: T) => unknown : never) extends (x: infer R) => unknown ? R : never
 
     type UnionToTuple<T> = UnionToIntersection<T extends any ? () => T : never> extends () => infer ReturnType ? [...UnionToTuple<Exclude<T, ReturnType>>, ReturnType] : []
 
 
     type a = UnionToTuple<1 | 2 | { a: 23 }>
+  }
+
+  // <------------------------------------------------>
+  // 7. a=1&b=2&c=3&d=4
+  type MergeParams<OneParam extends Record<string, any>, OtherParam extends Record<string, any>> = {
+    [Key in keyof OneParam | keyof OtherParam]: Key extends keyof OneParam ? Key extends keyof OtherParam ? OneParam[Key] extends keyof OtherParam[Key] ? OneParam[Key] : [OneParam[Key], OtherParam[Key]] : OneParam[Key] : Key extends keyof OtherParam ? OtherParam[Key] : never
+  }
+  type ParseParm<T extends string> = T extends `${infer Fir}=${infer Res}` ? { [key in Fir]: Res } : {}
+  type ParseQueryString<Str extends string> = Str extends `${infer Fir}&${infer Res}` ? MergeParams<ParseParm<Fir>, ParseQueryString<Res>> : ParseParm<Str>
+
+  {
+
+    type a = ParseQueryString<'a=1&b=2&c=3&d=4'>
   }
 }
